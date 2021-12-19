87c9abe27425651e01d71d60a26d2acad48ad731
robbe van wortswinkel <robbe.vanwortswinkel@ravago.com>
2021-12-19 19:02:53 +0100

    A

--- src/main/java/be/robbevw/swaggerdemo/controllers/TaskController.java
+++ src/main/java/be/robbevw/swaggerdemo/controllers/TaskController.java
@@ -1,27 +1,37 @@
 package be.robbevw.swaggerdemo.controllers;
 
 import be.robbevw.swaggerdemo.services.TaskService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import org.openapitools.api.TasksApiDelegate;
 import org.openapitools.model.Task;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
 @RequiredArgsConstructor
+@RestController
+@RequestMapping("/tasks")
+@Service
 public class TaskController implements TasksApiDelegate {
 
     private final TaskService taskService;
 
     @Override
+    @GetMapping
     public ResponseEntity<List<Task>> tasksGet() {
         return ResponseEntity.ok(taskService.findAll());
     }
 
     @Override
+    @GetMapping("/{id}")
     public ResponseEntity<Task> tasksIdGet(Long id) {
         Optional<Task> task = taskService.findById(id);
 
@@ -29,7 +39,8 @@ public class TaskController implements TasksApiDelegate {
     }
 
     @Override
-    public ResponseEntity<Void> tasksPost(Task task) {
+    @PostMapping
+    public ResponseEntity<Void> tasksPost(@ModelAttribute Task task) {
         taskService.save(task);
 
         return ResponseEntity.ok().build();
